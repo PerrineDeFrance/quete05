9262caac3f55464132b62936974a584216f0a02f
PerrineDeFrance <perrinehuon@gmail.com>
2023-02-22 09:23:41 +0100

    new

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css'],
+})
+export class SignUpComponent {
+  model: User = new User('', '', '', '');
+
+  constructor() {}
+
+  onSubmit(): void {
+    console.log(this.model);
+  }
+}
+
+export class User {
+  constructor(
+    public firstname: string,
+    public lastname: string,
+    public email: string,
+    public password: string
+  ) {}
+}
